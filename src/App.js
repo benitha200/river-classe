8a9b13288bf8595bb7611a186a8860f2705cc998
benitha200 <101089980+benitha200@users.noreply.github.com>
2023-01-10 14:13:21 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Slider from './components/DriverSupport/Slider';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import BarRestoSlider from './components/BarResto/BarRestoSlider';
+import DriverSupport from './components/DriverSupport/DriverSupport';
+import Footer from './components/Footer/Footer';
+import {useRef} from 'react';
+
+// style
+
+import '../src/assets/css/bootstrap.min.css'
 
 function App() {
+
+  const ref = useRef(null);
+  const handleClick = () =>{
+    ref.current?.scrollIntoView({behavior:'smooth'});
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route index element= {<DriverSupport ref={ref} handleClick={handleClick}/>}/>
+      <Route path="BarResto" element= {<BarRestoSlider/>}/>
+      <Route path="Tour" element= {<Slider handleClick={handleClick}/>}/>
+      <Route path="Supply" element= {<BarRestoSlider/>}/>  
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
